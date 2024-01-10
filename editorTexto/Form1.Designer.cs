a23b793456f95bbca4113db95a08499950754df4
anton <anton@DANY>
2024-01-10 15:15:44 -0600

    Agregar archivos de proyecto.

---
+++ editorTexto/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿using System.Globalization;
+using System.Windows.Forms;
+
+namespace editorTexto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TopMenuTools = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarComoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TopMenuTools.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 31);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(776, 375);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "Bienvenido";
+            this.richTextBox1.LanguageOption = RichTextBoxLanguageOptions.UIFonts;
+            this.richTextBox1.SelectionChanged += new System.EventHandler(this.richTextBox1_SelectionChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Location = new System.Drawing.Point(12, 413);
+            this.label1.MaximumSize = new System.Drawing.Size(78, 28);
+            this.label1.MinimumSize = new System.Drawing.Size(78, 28);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(78, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ln: 1, Col: 1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // TopMenuTools
+            // 
+            this.TopMenuTools.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem});
+            this.TopMenuTools.Location = new System.Drawing.Point(0, 0);
+            this.TopMenuTools.Name = "TopMenuTools";
+            this.TopMenuTools.Size = new System.Drawing.Size(800, 24);
+            this.TopMenuTools.TabIndex = 1;
+            this.TopMenuTools.Text = "TopMenuTools";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.guardarToolStripMenuItem,
+            this.guardarComoToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            this.guardarToolStripMenuItem.Click += new System.EventHandler(this.guardarToolStripMenuItem_Save);
+            // 
+            // guardarComoToolStripMenuItem
+            // 
+            this.guardarComoToolStripMenuItem.Name = "guardarComoToolStripMenuItem";
+            this.guardarComoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.guardarComoToolStripMenuItem.Text = "Guardar Como...";
+            this.guardarComoToolStripMenuItem.Click += new System.EventHandler(this.guardarComoToolStripMenuItem_Save);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.TopMenuTools);
+            this.MainMenuStrip = this.TopMenuTools;
+            this.MinimumSize = new System.Drawing.Size(400, 200);
+            this.Name = "Form1";
+            this.Text = "El Danytor - Editor de texto";
+            this.TopMenuTools.ResumeLayout(false);
+            this.TopMenuTools.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private MenuStrip TopMenuTools;
+        private ToolStripMenuItem archivoToolStripMenuItem;
+        private ToolStripMenuItem abrirToolStripMenuItem;
+        private ToolStripMenuItem guardarToolStripMenuItem;
+        private ToolStripMenuItem guardarComoToolStripMenuItem;
+        private ToolStripMenuItem salirToolStripMenuItem;
+    }
+}
+
---
+++ editorTexto/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+namespace editorTexto
+{
+    public partial class Form1 : Form
+    {
+        private OpenFileDialog openFileDialog;
+        private string fileUbication = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void richTextBox1_SelectionChanged(object sender, EventArgs e)
+        {
+            int posicion = richTextBox1.SelectionStart;
+            int linea = richTextBox1.GetLineFromCharIndex(posicion);
+            int columna = posicion - richTextBox1.GetFirstCharIndexFromLine(linea);
+            label1.Text = "Ln: " + (linea + 1) + ", Col: " + (columna + 1);
+            label1.Invalidate();
+        }
+
+
+
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog = new OpenFileDialog();
+            // Configurar el OpenFileDialog
+            openFileDialog.Filter = "Archivos de texto (*.txt)|*.txt |Archivos Json (*.json)|*.json";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true;
+
+            // Mostrar el cuadro de diálogo
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Obtener la ruta del archivo seleccionado
+                string filePath = openFileDialog.FileName;
+                fileUbication = filePath;
+                //Trasladando texto a richtextbox
+                try
+                {
+                    // Lee el contenido del archivo y lo carga en el RichTextBox
+                    string contenido = File.ReadAllText(filePath);
+                    richTextBox1.Text = contenido;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error al cargar el archivo: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        private void guardarToolStripMenuItem_Save(object sender, EventArgs e)
+        {
+            File.WriteAllText(fileUbication, richTextBox1.Text);
+        }
+
+        private void guardarComoToolStripMenuItem_Save(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.RestoreDirectory = false;
+            saveFileDialog.Filter = "Todos los Archivos |*.*";
+            saveFileDialog.FilterIndex = 1;
+            saveFileDialog.FileName = "Nuevo";
+            if (fileUbication != "")
+            {
+                saveFileDialog.InitialDirectory = fileUbication;
+                saveFileDialog.FileName = fileUbication;
+            }
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(fileUbication, richTextBox1.Text);
+            }
+        }
+    }
+}

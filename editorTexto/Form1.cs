01edc8cb59a4e08131f0a97ec48fc2532daf6423
antonydany <antonydany10@hotmail.com>
2024-01-18 23:57:47 -0600

    -Se agrega el zoomFactor para que el scroll del mouse y los botones de zoom en "vista" tengan el mismo comportamiento -en RichTextBox1_SelectionChanged ahora muestra los cambios de las columnas y las lineas cuando se selecciona hacia adelante -se crean cambian funciones que usaban eventos por funciones booleanas -se agrega las funciones de zoom, alejar, acercar y restablecer

--- editorTexto/Form1.cs
+++ editorTexto/Form1.cs
@@ -13,11 +13,14 @@ namespace editorTexto
     {
         private OpenFileDialog openFileDialog;
         readonly string windowTitle = "EditordeTexto v0.0.2(alpha) - ";
-        
+
         string fileUbication = "";
         string fileNombre = "Documento de Texto";
         private bool changesNotSaves = false;
         string selectedText = "";
+        //zoomFactor
+        readonly float zoomFactorMax = 12f;
+        readonly float zoomFactorMin = 0.5f;
 
         public Form1()
         {
@@ -30,15 +33,18 @@ namespace editorTexto
             {
                 changesNotSaves = true;
             }
+
         }
 
         private void RichTextBox1_SelectionChanged(object sender, EventArgs e)
         {
-            int posicion = richTextBox1.SelectionStart;
+            int posicion = richTextBox1.SelectionStart + richTextBox1.SelectionLength;
             int linea = richTextBox1.GetLineFromCharIndex(posicion);
             int columna = posicion - richTextBox1.GetFirstCharIndexFromLine(linea);
             this.lineColumnLabel.Text = $"Ln: {linea + 1}, Col: {columna + 1}";
+            RichTextBox1_SelectedCount(sender, e);
         }
+
         private void RichTextBox1_SelectedCount(object sender, EventArgs e)
         {
             int countCharacters = richTextBox1.SelectionLength;
@@ -79,54 +85,16 @@ namespace editorTexto
                 }
             }
         }
-        private void GuardarToolStripMenuItem_Save(object sender, EventArgs e)
+
+
+        private void GuardarToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (!string.IsNullOrEmpty(fileUbication))
-            {
-                File.WriteAllText(fileUbication, richTextBox1.Text);
-            }
-            else
-            {
-                SaveFileDialog saveFileDialog = new SaveFileDialog
-                {
-                    RestoreDirectory = true,
-                    Filter = "Archivo de Texto (*.txt)| *.txt|Todos los Archivos | *.*",
-                    FilterIndex = 1,
-                    FileName = "Documento de Texto"
-                };
-                if (saveFileDialog.ShowDialog() == DialogResult.OK)
-                {
-                    SaveFile(saveFileDialog.FileName);
-                }
-                // Console.WriteLine("Se guardó: " + fileNombre);
-            }
+            GuardarToolStripMenuItem_Save();
         }
-
-        private void SaveFile(string filePath)
+        private void GuardarComoToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            File.WriteAllText(filePath, richTextBox1.Text);
-            fileNombre = Path.GetFileName(filePath);
-            this.Text = windowTitle + fileNombre;
-            changesNotSaves = false;
+            GuardarComoToolStripMenuItem_Save();
         }
-
-        private void GuardarComoToolStripMenuItem_Save(object sender, EventArgs e)
-        {
-            SaveFileDialog saveFileDialog = new SaveFileDialog
-            {
-                RestoreDirectory = true,
-                Filter = "Archivo de Texto (*.txt)| *.txt|Todos los Archivos | *.*",
-                FilterIndex = 1,
-                FileName = fileNombre,
-            };
-
-            if (saveFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                SaveFile(saveFileDialog.FileName);
-                Console.WriteLine("Se guardó como: " + fileNombre);
-            }
-        }
-
 
 
         private void SalirToolStripMenuItem_Click(object sender, EventArgs e)
@@ -139,45 +107,26 @@ namespace editorTexto
             if (changesNotSaves)
             {
                 DialogResult result = MessageBox.Show("¿Quieres guardar los cambios antes de salir?", "Guardar cambios", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
-
                 if (result == DialogResult.Yes)
                 {
-                    if (fileUbication != "")
-                    {
-                        File.WriteAllText(fileUbication, richTextBox1.Text);
-                    }
-                    else
-                    {
-                        SaveFileDialog saveFileDialog = new SaveFileDialog
-                        {
-                            RestoreDirectory = true,
-                            Filter = "Archivo de Texto (*.txt)| *.txt|Todos los Archivos | *.*",
-                            FilterIndex = 1,
-                            FileName = "Documento de Texto",
-                        };
-                        if (saveFileDialog.ShowDialog() == DialogResult.OK)
-                        {
-                            fileNombre = Path.GetFileName(saveFileDialog.FileName);
-                            File.WriteAllText(saveFileDialog.FileName, richTextBox1.Text);
-                            // Actualiza el titulo
-                            this.Text = windowTitle + fileNombre;
-                            changesNotSaves = false;
-                            Console.WriteLine("Se guardó como: " + fileNombre);
-                        }
-                        else
-                        {
-                            // Cancelar el cierre si el usuario elige cancelar el guardado
-                            e.Cancel = true;
-                        }
-                    }
+                    e.Cancel = GuardarToolStripMenuItem_Save();
                 }
                 else if (result == DialogResult.Cancel)
                 {
-                    // Cancelar el cierre si el usuario elige cancelar el guardado
                     e.Cancel = true;
                 }
+                else
+                {
+                    e.Cancel = false;
+                }
+            }
+            else
+            {
+                e.Cancel = false;
             }
         }
+
+
 
         private void CortarToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -223,6 +172,102 @@ namespace editorTexto
             Busqueda busqueda = new Busqueda(richTextBox1);
             busqueda.Show();
         }
+
+        private void RestaurarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.richTextBox1.ZoomFactor = 1;
+        }
+
+        private void HandleZoom(float factor)
+        {
+            this.richTextBox1.ZoomFactor = Math.Max(0.5f, Math.Min(zoomFactorMax, this.richTextBox1.ZoomFactor + factor));
+            this.alejarToolStripMenuItem.Enabled = (this.richTextBox1.ZoomFactor > zoomFactorMin);
+            this.acercarToolStripMenuItem.Enabled = (this.richTextBox1.ZoomFactor < zoomFactorMax);
+        }
+
+        private void AcercarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            HandleZoom(0.1f);
+        }
+
+        private void AlejarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            HandleZoom(-0.1f);
+        }
+
+        private void RichTextBox1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (Control.ModifierKeys == Keys.Control)
+            {
+                float factor = e.Delta > 0 ? 0.1f : -0.1f;
+                HandleZoom(factor);
+            }
+        }
+
+
+        //funciones booleanas
+
+        private bool SaveFile(string filePath)
+        {
+            try
+            {
+                File.WriteAllText(filePath, richTextBox1.Text);
+                fileUbication = filePath;
+                fileNombre = Path.GetFileName(filePath);
+                this.Text = windowTitle + fileNombre;
+                changesNotSaves = false;
+                return false;
+            }
+            catch
+            {
+                return true;
+            }
+
+
+        }
+
+        private bool GuardarToolStripMenuItem_Save()
+        {
+            if (string.IsNullOrEmpty(fileUbication))
+            {
+                return GuardarComoToolStripMenuItem_Save();
+            }
+            else
+            {
+                return SaveFile(fileUbication);
+            }
+
+        }
+
+        private bool GuardarComoToolStripMenuItem_Save()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                RestoreDirectory = true,
+                Filter = "Archivo de Texto (*.txt)| *.txt|Todos los Archivos | *.*",
+                FilterIndex = 1,
+                FileName = fileNombre,
+            };
+            DialogResult dialogResult = saveFileDialog.ShowDialog();
+
+            if (dialogResult == DialogResult.OK)
+            {
+                Console.WriteLine("Se guardó como: " + fileNombre);
+                return SaveFile(saveFileDialog.FileName);
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                Console.WriteLine("Se presiono NO");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Se presiono otra cosa");
+                return true;
+            }
+
+        }
+
 
     }
 }
